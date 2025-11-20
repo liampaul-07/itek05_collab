e766f4aed452229167f100b9077f1faa025e37d7
Liam Raboy <lr.main0730@gmail.com>
2025-11-21 02:49:22 +0800

    Added order files

--- Final Outputs/food-kiosk-api/server.js
+++ Final Outputs/food-kiosk-api/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const foodRoutes = require('./src/routes/foodRoutes');
 const categoryRoutes = require('./src/routes/categoryRoutes');
-require('./src/config/database');
+const orderRoutes = require('./src/routes/orderRoutes');
+const db = require('./src/config/database');
 
 const app = express();
 const PORT = 3000;
@@ -10,9 +11,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/api/fooditems', foodRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`Database configuration loaded.`);
-    console.log(`Test your GET endpoint at: http://localhost:${PORT}/api/fooditems`);
 });
